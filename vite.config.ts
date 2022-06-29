0a4898c86cf099f3062d4a51e1272516d37aece0
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-06-29 23:46:51 +0700

    add fonts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        popup: 'popup/index.html',
-      },
-    },
-  },
   plugins: [react(), crx({ manifest })],
 })
