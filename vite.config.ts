90c50062d363c1dec9bbcafb1dffb1035ebfc653
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-06-30 00:33:16 +0700

    public dir base on mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config'
 
-export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+export default defineConfig(async ({ mode }) => {
+  const { EDHS_MODE } = loadEnv(mode, process.cwd(), 'EDHS') as { EDHS_MODE: string }
+
+  const publicDir = ['STAGING', 'DEVELOPMENT', 'PRE-RELEASE', 'PRERELEASE'].includes(EDHS_MODE?.toUpperCase() || '')
+    ? 'public-prerelease'
+    : 'public'
+
+  return {
+    publicDir,
+    plugins: [react(), crx({ manifest })],
+  }
 })
